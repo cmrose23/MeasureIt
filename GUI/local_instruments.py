97b51e57cf7cb0d2eb049ba58bfda25ec9a3298b
nanouser <nanophys@uw.edu>
2021-01-19 17:21:12 -0800

    added big blue instruments

--- GUI/local_instruments.py
+++ GUI/local_instruments.py
@@ -1,15 +1,19 @@
 from src.daq_driver import Daq
-from qcodes.instrument_drivers.Lakeshore.Model_372 import Model_372
-from qcodes.instrument_drivers.american_magnetics.AMI430 import AMI430
 from qcodes.instrument_drivers.stanford_research.SR860 import SR860
 from qcodes.instrument_drivers.stanford_research.SR830 import SR830
 from qcodes.instrument_drivers.tektronix.Keithley_2450 import Keithley2450
+from qcodes_contrib_drivers.drivers.Oxford.IPS120 import OxfordInstruments_IPS120
+from src.Model_340 import Model_340
+from src.ITC503 import ITC503
+from src.LM510 import LM510
 
 # To add an instrument, import the driver then add it to our instrument
 # dictionary with the name as the key, and the class as the value
 LOCAL_INSTRUMENTS = {'NI DAQ': Daq,
-                     'Model_372': Model_372,
-                     'AMI430': AMI430,
+                     'IPS120': OxfordInstruments_IPS120,
+                     'ITC503': ITC503,
+                     'LM510': LM510,
+                     'Model 340': Model_340,
                      'SR860': SR860,
                      'SR830': SR830,
                      'Keithley2450': Keithley2450}
