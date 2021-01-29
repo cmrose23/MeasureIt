bf3edf3752653716a0cbb548a0ffd042ed9482c3
nanouser <nanophys@uw.edu>
2021-01-28 17:29:16 -0800

    setup for big blue computer

--- MeasureIt.bat
+++ MeasureIt.bat
@@ -1,3 +1,3 @@
 call conda.bat activate qcodes
-C:\Users\erunb\Anaconda3\envs\qcodes\python.exe C:\Users\erunb\MeasureIt\GUI\GUI_Measureit.py
+C:\Users\nanouser\Anaconda3\envs\qcodes\python.exe C:\Users\nanouser\Documents\MeasureIt\GUI\GUI_Measureit.py
 call conda deactivate
