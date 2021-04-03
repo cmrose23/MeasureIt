74af2f43e9689e2d8af7ffaedbd9b94f68f4b3d0
nanouser <nanophys@uw.edu>
2021-04-03 15:29:03 -0700

    correct path for big blue

--- MeasureIt.bat
+++ MeasureIt.bat
@@ -1,3 +1,3 @@
 call conda.bat activate qcodes
-C:\Users\nanouser\Anaconda3\envs\qcodes\python.exe C:\Users\nanouser\Documents\MeasureIt\GUI\GUI_Measureit.py
+C:\Users\nanouser\Anaconda3\envs\qcodes\python.exe C:\Users\nanouser\MeasureIt\GUI\GUI_Measureit.py
 call conda deactivate
