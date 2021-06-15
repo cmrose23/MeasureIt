5be6af3852ad447634d5d181ef0849f562979a8d
Jiaqi Cai <caidish1234@gmail.com>
2021-06-15 13:31:45 -0700

    Update local_instruments.py
    Add Mock Test instrument

--- GUI/local_instruments.py
+++ GUI/local_instruments.py
@@ -4,7 +4,7 @@ from qcodes.instrument_drivers.american_magnetics.AMI430 import AMI430
 from qcodes.instrument_drivers.stanford_research.SR860 import SR860
 from qcodes.instrument_drivers.stanford_research.SR830 import SR830
 from qcodes.instrument_drivers.tektronix.Keithley_2450 import Keithley2450
-
+from qcodes.tests.instrument_mocks import MockParabola
 # To add an instrument, import the driver then add it to our instrument
 # dictionary with the name as the key, and the class as the value
 LOCAL_INSTRUMENTS = {'NI DAQ': Daq,
@@ -12,4 +12,5 @@ LOCAL_INSTRUMENTS = {'NI DAQ': Daq,
                      'AMI430': AMI430,
                      'SR860': SR860,
                      'SR830': SR830,
-                     'Keithley2450': Keithley2450}
+                     'Keithley2450': Keithley2450,
+                     'Test':MockParabola}
